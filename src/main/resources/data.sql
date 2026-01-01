567767b13925b7386ee7a9a056fb1424410e8891
ReimoSi <122689064+ReimoSi@users.noreply.github.com>
2026-01-01 13:58:02 +0200

    chore(db): seed initial sample data for draws

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO draws (draw_id, draw_date, main_numbers, bonus_numbers)
+VALUES ('2025-12-31', '2025-12-31', '1 15 21 22 29 46', '1'),
+       ('2025-12-24', '2025-12-24', '8 9 28 30 36 43', '2');
