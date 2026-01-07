63310c7f0b40e4ce3a1b6d7d5d2fea4cc8b28057
ReimoSi <122689064+ReimoSi@users.noreply.github.com>
2026-01-07 22:16:32 +0200

    chore: configure IntelliJ DB source (HSQL TCP on 9001) and attach SQL files

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 -- generated from CSV
-INSERT INTO draws (draw_id, draw_date, main_numbers, bonus_numbers) VALUES ('2024-01-03', '2024-01-03', '1 4 7 12 33 40', '2');
-INSERT INTO draws (draw_id, draw_date, main_numbers, bonus_numbers) VALUES ('2024-01-10', '2024-01-10', '3 8 11 19 27 44', '5');
+INSERT INTO draw (draw_id, draw_date, main_numbers, bonus_numbers)
+VALUES ('2024-01-03', '2024-01-03', '1 4 7 12 33 40', '2');
+
+INSERT INTO draw (draw_id, draw_date, main_numbers, bonus_numbers)
+VALUES ('2024-01-10', '2024-01-10', '3 8 11 19 27 44', '5');
